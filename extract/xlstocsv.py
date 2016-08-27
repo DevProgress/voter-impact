877ac0d8ad3a9bae35f40624fac1f9cb95c0070c
Nelson Minar <nelson@monkey.org>
2016-08-27 01:32:48 +0000

    parse 2014 data

--- extract/xlstocsv.py
+++ extract/xlstocsv.py
@@ -1,47 +1,57 @@
 #!/usr/bin/env python3
 
-"""Create a CSV showing 2012 Congress election results
-Source data: http://www.fec.gov/pubrec/fe2012/federalelections2012.shtml
+"""Create a CSV showing 2014 Congress election results
+Source data: http://www.fec.gov/pubrec/fe2014/federalelections2014.shtml
 This script emits general election results for House distracts and senate races"""
+
+# this code was created with cut-and-paste from xlstocsv.py. Needs refactoring :-(
 
 import xlrd
 import csv, sys
 
-src_fn = '../data/federalelections2012.xls'
-src_sheet = '2012 US House & Senate Resuts'
 
-### File IO
-wb = xlrd.open_workbook(src_fn)
-sheet = wb.sheet_by_name(src_sheet)
-rows = sheet.get_rows()
-out = csv.writer(sys.stdout, dialect='excel-tab', quoting=csv.QUOTE_NONE, quotechar=None)
+for src_fn, dst_fn, src_sheets in (
+    ('../data/federalelections2012.xls', '../data/2012-congress.tsv', ('2012 US House & Senate Resuts',)),
+    ('../data/results14.xls', '../data/2014-congress.tsv', ('2014 US Senate Results by State', '2014 US House Results by State')),
 
-### CSV schema / headers
-# Source schema
-headers = [str(cell.value) for cell in next(rows)]
-col_index = {headers[i] : i for i in range(len(headers))}
-# Output schema
-out.writerow(['State', 'District', 'First Name', 'Last Name', 'Party', 'Votes', 'Vote fraction', 'Winner'])
+):
+    print("Converting %s to %s" % (src_fn, dst_fn))
+    with open(dst_fn, 'w') as fp:
+        ### File IO
+        wb = xlrd.open_workbook(src_fn)
+        out = csv.writer(fp, dialect='excel-tab', quoting=csv.QUOTE_NONE, quotechar=None)
 
-def v(r, name):
-    "Extract the value for row r with column name n. Coerces to string."
-    return str(r[col_index[name]].value).strip()
+        # Output schema
+        out.writerow(['State', 'District', 'First Name', 'Last Name', 'Party', 'Votes', 'Vote fraction', 'Winner'])
 
-### Iterate through every row of data
-for row in rows:
-    # See if a useful district is named in column D; this contains vote data
-    district = v(row, 'D')
-    if len(district) > 0 and district != 'H':
-        # Looks like a useful record; emit the columns we care about
-        votes = row[col_index['GENERAL VOTES ']].value
-        fractVote = row[col_index['GENERAL %']].value
-        if fractVote != "" and fractVote > 0:
-            out.writerow([
-                v(row, 'STATE ABBREVIATION'),
-                v(row, 'D'),
-                v(row, 'CANDIDATE NAME (First)'),
-                v(row, 'CANDIDATE NAME (Last)'),
-                v(row, 'PARTY'),
-                "%d" % votes,
-                "%.5f" % fractVote,
-                v(row, 'GE WINNER INDICATOR')])
+        for sheet_name in src_sheets:
+            sheet = wb.sheet_by_name(sheet_name)
+            rows = sheet.get_rows()
+
+            ### CSV schema / headers
+            # Source schema
+            headers = [str(cell.value) for cell in next(rows)]
+            col_index = {headers[i] : i for i in range(len(headers))}
+
+            def v(r, name):
+                "Extract the value for row r with column name n. Coerces to string."
+                return str(r[col_index[name]].value).strip()
+
+            ### Iterate through every row of data
+            for row in rows:
+                # See if a useful district is named in column D; this contains vote data
+                district = v(row, 'D')
+                if len(district) > 0 and district != 'H':
+                    # Looks like a useful record; emit the columns we care about
+                    votes = row[col_index['GENERAL VOTES ']].value
+                    fractVote = row[col_index['GENERAL %']].value
+                    if fractVote != "" and fractVote > 0:
+                        out.writerow([
+                            v(row, 'STATE ABBREVIATION'),
+                            v(row, 'D'),
+                            v(row, 'CANDIDATE NAME (First)'),
+                            v(row, 'CANDIDATE NAME (Last)'),
+                            v(row, 'PARTY'),
+                            "%d" % votes,
+                            "%.5f" % fractVote,
+                            v(row, 'GE WINNER INDICATOR')])
